40c21fb4ad353511797c13fda4d9d7918f97155c
ChinoMandarin <francofedullo22@gmail.com>
2023-07-14 18:23:38 -0300

    Listado Terminado con paginacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,17 +6,34 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 use App\Models\post;
-
+use App\Models\author;
 class PostController extends Controller
 {
     public function List(Request $request){
+        $posts = post::paginate(3);
+        foreach ($posts as $post) {
 
+            $authorId = $post['author'];
+
+            $author= author::findOrFail($authorId);
+            $authorName = $author['name'];
+            
+            $post['author'] = $authorName;
+
+        }
+        
         return view("listPost",[ 
-            'post' => post::all()
+            'post' => $posts
         ]);
     }
 
     public function Create(Request $request){
-        
+
+    }
+
+
+    public function index()
+    {
+        return view('post.index');
     }
 }
---
+++ resources/views/listPost.blade.php
@@ -0,0 +1,70 @@
+<style>
+    .post-list {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .post-item {
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        padding: 10px;
+        background-color: #f9f9f9;
+    }
+
+    .post-title {
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    .post-author {
+        color: #666;
+    }
+
+    .pagination {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .pagination .page-item {
+        margin: 0 5px;
+        list-style-type: none;
+    }
+
+    .pagination .page-item .page-link {
+        padding: 5px 10px;
+        background-color: #f9f9f9;
+        border: 1px solid #ccc;
+        color: #333;
+        text-decoration: none;
+    }
+
+    .pagination .page-item .page-link:hover {
+        background-color: #ccc;
+    }
+
+    .pagination .page-item.active .page-link {
+        background-color: #333;
+        color: #fff;
+    }
+
+</style>
+
+<h1>Lista de Posts</h1>
+
+<ul class="post-list">
+    @foreach ($post as $pos)
+        <li class="post-item">
+            <div class="post-title">Título:</div>
+            <div>{{ $pos->title }}</div>
+            <div class="post-title">Contenido:</div>
+            <div>{{ $pos->body }}</div>
+            <div class="post-title">Autor:</div>
+            <div class="post-author">{{ $pos->author }}</div>
+        </li>
+    @endforeach
+</ul>
+<div class="pagination">
+{{ $post->links() }}
+
